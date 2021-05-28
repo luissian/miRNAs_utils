5696dab1f430002f21c32f4ce72a7d1377bd7c3e
luissian <chapado.l@gmail.com>
2021-05-28 23:10:22 +0200

    Blast with the forward reads

--- blast_writer.py
+++ blast_writer.py
@@ -16,32 +16,42 @@ class BlastWriter(DataWriter):
         return super().pre()
 
     def post(self):
-        num_match = 0
+        num_match = [0,0]
         not_match_num = 0
-        match_file = os.path.join(self.__foldername, 'found_matches.tsv')
+        kind_matches =['direct','compl']
+        match_file_direct = os.path.join(self.__foldername, 'found_matches_forward_direction.tsv')
+        match_file_compl = os.path.join(self.__foldername, 'found_matches_complementary.tsv')
         no_match_file = os.path.join(self.__foldername, 'no_matches_found.txt')
         try:
-            with open(match_file,'w') as fh_match, open(no_match_file, 'w') as fh_no_match :
-                fh_match.write(self.__match_heading + '\n')
+            with open(match_file_direct,'w') as fh_match_direct, open(match_file_compl,'w') as fh_match_compl, open(no_match_file, 'w') as fh_no_match  :
+                fh_match_direct.write(self.__match_heading + '\n')
+                fh_match_compl.write(self.__match_heading + '\n')
                 fh_no_match.write('Organism\n')
                 for elem in self.__buffer:
-                    for key, values in elem.items():
-                        if values != '':
-
-                            # print('species : ' , key)
-                            for value in values:
-                                # print(value)
-                                fh_match.write(str(key + '\t'+ value + '\n'))
-                            num_match +=1
-                        else:
+                    for key in elem.keys():
+                        found_any_match = 0
+                        for k_match in kind_matches:
+                            if elem[key][k_match] != '':
+                                found_any_match = 1
+                                for value in elem[key][k_match]:
+                                    if k_match == 'direct':
+                                    # print(value)
+                                        fh_match_direct.write(str(key + '\t'+ value + '\n'))
+                                        num_match[0] +=1
+                                    else:
+                                        fh_match_compl.write(str(key + '\t'+ value + '\n'))
+                                        num_match[1] +=1
+                        if found_any_match == 0:
                             fh_no_match.write(str(key + '\n'))
                             not_match_num +=1
         except:
             print('unable to create the output files')
             exit(2)
         print('\nSummary\n')
-        print('Organisms found ', str(num_match))
-        print('Number of Organisms with not match : ',str(not_match_num))
+        print("Organisms found in 5' -- 3' direction: ", str(num_match[0]))
+        print("Organisms found with complementary miRNA: ", str(num_match[1]))
+        print('Number of Organisms which not match in any of the above: ',str(not_match_num))
+        
 
 
 
